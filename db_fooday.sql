43db37305484995857c907565cff5151b0f95b7e
nguyenthiyenlinh <37701381+nguyenthiyenlinh@users.noreply.github.com>
2019-03-24 16:35:30 +0700

    Cơ sở dữ liệu đã qua chĩnh sửa hoàn chỉnh

--- db_fooday.sql
+++ db_fooday.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2019 at 10:14 AM
+-- Generation Time: Mar 24, 2019 at 09:59 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 5.6.40
 
@@ -41,7 +41,32 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
-(4, 'Category update', 'category-update', '2019-03-22 00:03:41', '2019-03-22 00:16:16');
+(5, 'Drink', 'drink', '2019-03-22 02:42:03', '2019-03-22 02:42:03'),
+(6, 'Food', 'food', '2019-03-22 02:42:14', '2019-03-22 02:42:14'),
+(7, 'Fruit', 'fruit', '2019-03-22 02:43:32', '2019-03-22 02:43:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `subject` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `email`, `subject`, `message`, `created_at`, `updated_at`) VALUES
+(2, 'BaekHo', 'baekho@nuest.com', 'test', 'điền vào nội dung message tại đây', '2019-03-23 23:51:01', '2019-03-23 23:51:01');
 
 -- --------------------------------------------------------
 
@@ -59,6 +84,16 @@ CREATE TABLE `items` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `category_id`, `name`, `description`, `price`, `image`, `created_at`, `updated_at`) VALUES
+(9, 5, 'Nước mía', 'ngon bổ rẻ', 5000, 'nuoc-mia-2019-03-23-5c95bf01c997b. jpg', '2019-03-22 22:07:13', '2019-03-22 22:07:13'),
+(11, 6, 'Bánh tráng trộn', 'ăn vặt', 10000, 'banh-trang-tron-2019-03-23-5c95bfc83ebb4.jpg', '2019-03-22 22:10:32', '2019-03-22 22:10:32'),
+(12, 7, 'Trái cây dĩa', 'Bổ sung nhiều chất dinh dưỡng', 15000, 'trai-cay-dia-2019-03-23-5c95c01093784.jpg', '2019-03-22 22:11:44', '2019-03-22 22:11:44'),
+(13, 5, 'Cà phê sữa đá', 'Giúp bạn tỉnh táo cả ngày', 10000, 'ca-phe-sua-da-2019-03-23-5c95c040d5c29.jpg', '2019-03-22 22:12:32', '2019-03-22 22:12:32');
 
 -- --------------------------------------------------------
 
@@ -81,7 +116,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_03_20_025851_create_sliders_table', 2),
 (4, '2019_03_22_050546_create_categories_table', 3),
-(5, '2019_03_22_071936_create_items_table', 4);
+(5, '2019_03_22_071936_create_items_table', 4),
+(6, '2019_03_23_133010_create_reservations_table', 5),
+(7, '2019_03_24_054957_create_contacts_table', 6);
 
 -- --------------------------------------------------------
 
@@ -94,6 +131,34 @@ CREATE TABLE `password_resets` (
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_and_time` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `message` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `name`, `phone`, `email`, `date_and_time`, `message`, `status`, `created_at`, `updated_at`) VALUES
+(4, 'Luc Thao', '0779882660', 'admin@admin.com', '24 March 2019 - 12:11 AM', 'test message', 1, '2019-03-23 11:13:13', '2019-03-23 21:15:37'),
+(5, 'Minhyun', '0123456789', 'minhyun@nuest.com', '25 March 2019 - 10:11 AM', 'cwdscrgv', 0, '2019-03-23 11:14:03', '2019-03-23 11:14:03'),
+(6, 'abc', '963258741', 'abcxyz@gmail.com', '02 March 2019 - 09:11 AM', 'ftrgsfxt', 0, '2019-03-23 11:17:02', '2019-03-23 11:17:02'),
+(7, 'Luc An', '123456', 'abc@gmail.com', '25 March 2019 - 12:11 PM', 'fweffcee', 0, '2019-03-23 22:45:51', '2019-03-23 22:45:51');
 
 -- --------------------------------------------------------
 
@@ -115,7 +180,7 @@ CREATE TABLE `sliders` (
 --
 
 INSERT INTO `sliders` (`id`, `title`, `sub_title`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'test update', 'update tại đây nè', 'test-update-2019-03-21-5c934c67f1323.png', '2019-03-19 17:00:00', '2019-03-21 01:33:43'),
+(1, 'test update', 'update tại đây nè', 'test-update-2019-03-21-5c934c67f1323.png', '2019-03-19 17:00:00', '2019-03-22 02:31:07'),
 (3, 'banh', 'bánh tráng trộn', 'banh-2019-03-20-5c9285f9d1ba0.jpg', '2019-03-20 11:27:05', '2019-03-20 11:27:05'),
 (4, 'drink', 'Cà phê sữa đá', 'drink-2019-03-20-5c928b88ebf53.jpg', '2019-03-20 11:50:48', '2019-03-20 11:50:48'),
 (5, 'trái cây', 'daehan minguk mansae', 'trai-cay-2019-03-20-5c928ebbde0c5.jpg', '2019-03-20 12:04:27', '2019-03-20 12:04:27');
@@ -141,7 +206,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Luc Thao', 'admin@admin.com', '$2y$10$rhmnPZeZjUGeTNXkZSWZ5.Jk.TgA.WR.qE/ufGEKED8qy/WnbuAAq', 'GR84ouGHQTjVxiJ8aBTU5CP8nfYQn1Zel4y3F9YUtI2vQpl2BeSSXeCoB22W', '2019-03-19 16:33:49', '2019-03-19 16:33:49');
+(1, 'Luc Thao', 'admin@admin.com', '$2y$10$rhmnPZeZjUGeTNXkZSWZ5.Jk.TgA.WR.qE/ufGEKED8qy/WnbuAAq', 'rF4ebf3rk8PhozfAS2kiv20977kAWY6T1A6X3C8yzY4wFp80OawicHMucgaV', '2019-03-19 16:33:49', '2019-03-19 16:33:49');
 
 --
 -- Indexes for dumped tables
@@ -151,6 +216,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `creat
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -173,6 +244,12 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sliders`
 --
 ALTER TABLE `sliders`
@@ -193,19 +270,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `sliders`
